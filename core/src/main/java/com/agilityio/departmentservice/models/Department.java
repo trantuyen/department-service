38abe97e02a8a130eafe9fdd3c1e4a07bc9296aa
Tuyen Tran <tuyen.tran@asnet.com.vn>
2018-05-28 16:10:06 +0700

    Add department data model Add dependencies for core module

---
+++ core/src/main/java/com/agilityio/departmentservice/models/Department.java
@@ -0,0 +1,41 @@
+package com.agilityio.departmentservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Department {
+
+    /**
+     * The department id
+     */
+    @Id
+    private String id;
+
+    /**
+     * The department name
+     */
+    private String name;
+
+    /**
+     * The department phone number
+     */
+    private String phone;
+
+    /**
+     * The department address
+     */
+    private String address;
+}
