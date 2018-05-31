6af7eee41c4389c5dca6b9d0e204f645d72f27c0
Tuyen Tran <tuyen.tran@asnet.com.vn>
2018-05-31 15:35:27 +0700

    Rename modules

--- department-service-server/src/main/java/com/agilityio/departmentservice/DepartmentServiceApplication.java
+++ department-service-server/src/main/java/com/agilityio/departmentservice/DepartmentServiceApplication.java
@@ -3,11 +3,9 @@ package com.agilityio.departmentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan(basePackages = { "com.agilityio.departmentservice" })
 public class DepartmentServiceApplication {
 
     public static void main(String[] args) {
--- department-service-server/src/main/java/com/agilityio/departmentservice/models/DepartmentInternal.java
+++ department-service-server/src/main/java/com/agilityio/departmentservice/models/DepartmentInternal.java
@@ -1,6 +1,12 @@
 package com.agilityio.departmentservice.models;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
